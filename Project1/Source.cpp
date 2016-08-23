dc9ea076f60142d66d10fe39e21356d9a37dc52d
Conner-Stack <connerstack@gmail.com>
2016-08-23 14:13:15 -0500

    complete
    complete loops exercises.

---
+++ Project1/Source.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Example 1
+	//Part A
+	/*for (int i = 100; i >= 0; i--)
+		cout << i << endl;
+
+	system("pause");
+	return 0;*/
+
+	//Part B
+	/*for (int i = 100; i >= 0; i--)
+		cout << i << endl;
+
+	system("pause");
+	return 0;
+*/
+
+	//part C
+	/*for (int i = 99; i >= 0; i--)
+		cout << i << endl;
+
+	system("pause");
+	return 0;
+*/
+	//part D
+	/*for (int i = 100; i > 0; i--)
+		cout << i << endl;
+
+	system("pause");
+	return 0;*/
+
+	//Part E
+	/*for (int i = 100; i >= 0; i-=2)
+		cout << i << endl;
+
+	system("pause");
+	return 0;*/
+
+	//Example 2
+	//part a
+	
+	
+	/*for (int i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i > 0)
+		{
+			cout << "fizz" << endl;
+		}
+		else
+			cout << i << endl;
+}
+	system("pause");
+	return 0;*/
+	
+	//part 2
+
+	/*for (int i = 0; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i > 0)
+		{
+			cout << "buzz" << endl;
+		}
+		else
+			cout << i << endl;
+	}
+	system("pause");
+	return 0;*/
+
+	//part 3
+
+	/*for (int i = 0; i <= 100; i++)
+	{
+		if ((i % 3 == 0 || i % 5 == 0) && i > 0)
+		{
+			cout << "fizzbuzz" << endl;
+		}
+		else
+			cout << i << endl;
+	}
+	system("pause");
+	return 0;
+*/
+
+	//Example 3
+
+//	int sum5 = 0;
+//	int sum3 = 0;
+//
+//	//the sum of all multiples of 3 under 1000
+//	for (int i = 0; i < 1000; i += 3)
+//	{
+//		sum3 = sum3 + i;	
+//	}
+//cout << sum3 << endl;
+//	
+//	system("pause");
+//	return 0;
+//
+//	//the sum of all multiples of 5 under 1000
+//	for (int i = 0; i < 1000; i += 5)
+//	{
+//		sum5 = sum5 + i;
+//	}
+//	cout << sum5 << endl;
+//	system("pause");
+//	return 0;
+
+//Example 4
+
+//part A
+
+//int i = 101;
+//while (i > 0)
+//{
+//	i = i--;
+//		cout << i << endl;
+//}
+//system("pause");
+//return 0;
+
+//Part A.2
+
+//int i = 100;
+//while (i > 0)
+//{
+//	i = i--;
+//	cout << i << endl;
+//
+//}
+//system("pause");
+//return 0;
+
+//part A.3
+
+//int i = 101;
+//while (i > 1)
+//{
+//	i = i--;
+//	cout << i << endl;
+//}
+//system("pause");
+//return 0;
+//
+
+//part A.4
+
+//int i = 102;
+//while (i > 1)
+//{
+//	i = i-=2;
+//	cout << i << endl;
+//
+//}
+//system("pause");
+//return 0;
+
+//Part B
+
+//int i = -1;
+//
+//while (i < 100)
+//{
+//	i = i++;
+//
+//	if (i % 3 == 0)
+//		cout << "fizz" << endl;
+//	else
+//		cout << i << endl;
+//
+//}
+//system("pause");
+//return 0;
+
+//part B.2
+//int i = -1;
+//
+//while (i < 100)
+//{
+//	i = i++;
+//
+//	if (i % 5 == 0)
+//		cout << "buzz" << endl;
+//	else
+//		cout << i << endl;
+//
+//}
+//system("pause");
+//return 0;
+
+//part B.3
+//int i = -1;
+//
+//while (i < 100)
+//{
+//	i = i++;
+//
+//	if (i % 3 == 0 || i % 5 == 0)
+//		cout << "fizzbuzz" << endl;
+//	else
+//		cout << i << endl;
+//
+//}
+//system("pause");
+//return 0;
+
+//part C
+//int i = 0;
+//int sum = 0;
+//while (i < 999)
+//{ 
+//	i = i += 3;
+//	sum = sum + i;
+//}
+//cout << sum << endl;
+//system("pause");
+//return 0;
+
+//int i = 0;
+//int sum = 0;
+//while (i < 999)
+//{
+//	i = i += 5;
+//	sum = sum + i;
+//}
+//cout << sum << endl;
+//system("pause");
+//return 0;
+}
